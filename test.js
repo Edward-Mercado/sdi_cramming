7bb1b732c4d10cd80c61dd195001e72d5386060f
Edward-Mercado <edwardmercado149@gmail.com>
2025-06-29 17:14:18 -0400

    baby's first javascript
    it sucks.

--- test.js
+++ test.js
@@ -1,2 +1,13 @@
-console.log("HELLO WORLD!")
-console.log(1+1)
+function print(text) {
+    console.log(text);
+}
+
+function randint(low, high) {
+    range = high - low + 1;
+    let x = Math.floor(Math.random() * range);
+    x += low;
+    return x;
+}
+
+print("Hello World!");
+print(randint(1, 34));
